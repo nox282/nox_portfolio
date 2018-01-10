6be1d13d9bdd366595578e5b8df105dfea2527ed
Nicolas Hamard <nox.tux@gmail.com>
2018-01-09 22:12:05 -0500

    removing debug console.log in ajaxapp

--- src/main/resources/static/js/ajaxApplication.js
+++ src/main/resources/static/js/ajaxApplication.js
@@ -126,8 +126,6 @@ const renderResumeElements = (data) => {
 
 
 const renderHiringInfoElement = (data) => {
-    console.log(data);
-    console.log("hello");
     return `<div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2">
